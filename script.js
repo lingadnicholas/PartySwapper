3a6dd6e61c24c29da4a394b1f01c2473152ae9df
Nicholas Lingad <lingadnicholas@engineering.ucla.edu>
2023-10-30 17:40:33 -0700

    Removed party size field being dynamic (was not necessary, bad UX); Added dynamic field numbers for names

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let form = document.getElementById("party-form")
-let num_people = document.getElementById("num-people")
-
+let num_people_field = document.getElementById("num-people")
+let num_party_field = document.getElementById("num-party")
 function spinWheel() {
     // TODO 
 
     // First, validate form 
+    // party size < num people
+
 
     // Second, Populate array with people in party
 
@@ -15,20 +17,52 @@ function spinWheel() {
 
 // numbers should be positive integers 
 function validateNumber(num) {
-    return (Number.isInteger(num) && num > 0)
+    return (Number.isInteger(Number(num)) && Number(num) > 0)
 }
-// When number of people gets populated, add party size field
-num_people.addEventListener("input", (e) => {
-    e.preventDefault() 
-    if (validateNumber(Number(num_people.value))) {
-        document.getElementById("party-size-field").innerHTML = 
-        '<label>Party size: </label><br><input type="number" id="num-party"><br>'
-    }
-    else {
-        document.getElementById("party-size-field").innerHTML = 
-        ''
-    }
-})
 
+function fillNameFields() {
+    
+    if (num_people_field.value == null || num_party_field.value == null) {
+        return
+    }
+    if (!validateNumber(num_people_field.value) || !validateNumber(num_party_field.value)) {
+        console.log("LOG: Either number of people or party size are invalid")
+        return 
+    }
+    let num_people = num_people_field.value 
+    let num_party = num_party_field.value
+
+
+    function modify_num_fields(field, class_name, num) {
+        num_children = field.childElementCount 
+        if (num_children < num) { // Add text input fields  
+            to_add = num - num_children
+            console.log(to_add)
+            while (to_add > 0) {
+                let new_child = document.createElement("input")
+                new_child.type = "text" 
+                new_child.className = class_name
+                field.appendChild(new_child)
+                to_add-- 
+            }
+        } else if (num_children > num) { // Remove text input fields 
+            to_remove = num_children - num 
+            while (to_remove > 0) {
+                field.removeChild(field.lastElementChild) 
+                to_remove--
+            }
+        }
+    }
+    
+    modify_num_fields(document.getElementById("in-party-list"), "in-party-input", num_party)
+    modify_num_fields(document.getElementById("not-in-party-list"), "not-in-party-input", num_people-num_party)
+
+}
+
+
+// TODO: 
 // When number of people and party size are populated, add the correct number of fields for names
+num_people_field.addEventListener("input", (fillNameFields))
+num_party_field.addEventListener("input", (fillNameFields))
+
 form.addEventListener("submit", (spinWheel))
